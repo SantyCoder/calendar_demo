feac77687cacef8c5c0f2a75ebe8e2deff1679a5
Santiago Marcelino Flores <santiago.marcelino@gigigo.com>
2025-11-12 10:00:24 -0600

    update deploy model

---
+++ _nuxt/CXoovzYf.js
@@ -0,0 +1,2 @@
+import{_ as s}from"./DlAUqK2U.js";import{c as o,a as n,o as t}from"#entry";const a={},r={class:"w-full p-10"};function c(l,e){return t(),o("div",r,[...e[0]||(e[0]=[n("span",null,"McDonald's Games",-1)])])}const f=s(a,[["render",c]]);export{f as default};
+//# sourceMappingURL=CXoovzYf.js.map
